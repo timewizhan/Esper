78f60e3d684c5c202338da61f100d4930fce50d1
didvlwl <didvlwl@hanmail.net>
2015-11-30 13:02:46 +0900

    client upload

---
+++ Client/EsperClient/SettingDlg.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+// CSettingDlg 대화 상자입니다.
+
+class CSettingDlg : public CDialog
+{
+	DECLARE_DYNAMIC(CSettingDlg)
+
+public:
+	CSettingDlg(CWnd* pParent = NULL);   // 표준 생성자입니다.
+	virtual ~CSettingDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SETTING };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnBnClickedButton1();
+};
