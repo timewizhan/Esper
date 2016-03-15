b22ecd48fc2b9b15d8debd884c267156d34bd194
PIGMANIA <tkddn017@gmail.com>
2016-03-15 10:21:01 +0900

    [Client][PIGMANIA][RA:N/A] EsperOpenerRegistry update is added.

--- Client/EsperRegEdit/EsperRegEdit/main.cpp
+++ Client/EsperRegEdit/EsperRegEdit/main.cpp
@@ -42,6 +42,8 @@ void writeToRegistry(void)
 			&dwDisposition
 			);
 	}
+	else cout << "ESPER already exist" << endl;
+
 		//http://msdn.microsoft.com/en-us/library/ms724837(VS.85).aspx
 		RegCloseKey(hKey);
 	
@@ -92,6 +94,106 @@ void writeToRegistry(void)
 		RegCloseKey(hKey);
 	}
 
+// ----------------------       Esperopener ----------------------------------------------
+
+void writeToRegistry2(void)
+{
+	DWORD lRv;
+	HKEY hKey;
+	//First,create a key .esp and update the default value. 
+	//Check if the registry exists
+	//http://msdn.microsoft.com/en-us/library/ms724897(VS.85).aspx
+	lRv = RegOpenKeyEx(
+		HKEY_CLASSES_ROOT,
+		".esp",
+		0,
+		KEY_WRITE,
+		&hKey
+		);
+
+	if (lRv != ERROR_SUCCESS)
+	{
+		DWORD dwDisposition;
+
+		cout << ".esp key is created..." << endl;
+
+		// Create a key if it did not exist
+		//http://msdn.microsoft.com/en-us/library/ms724844(VS.85).aspx
+		lRv = RegCreateKeyEx(
+			HKEY_CLASSES_ROOT,
+			".esp", //"Use Multi-Byte Character Set" by using L
+			0,
+			"ESPER OPENER",
+			REG_OPTION_NON_VOLATILE,
+			KEY_ALL_ACCESS,
+			NULL,
+			&hKey,
+			&dwDisposition
+			);
+
+		RegSetValueEx(
+			hKey,
+			NULL,
+			0,
+			REG_SZ,
+			//reinterpret_cast<BYTE *>(&string),
+			(BYTE *)"EsperOpener_exe",
+			strlen("EsperOpener_exe")
+			);
+	}
+	//http://msdn.microsoft.com/en-us/library/ms724837(VS.85).aspx
+	RegCloseKey(hKey);
+
+
+	//Second,create a key 'command' 
+
+	//Check if the registry exists
+	//http://msdn.microsoft.com/en-us/library/ms724897(VS.85).aspx
+	lRv = RegOpenKeyEx(
+		HKEY_CLASSES_ROOT,
+		"EsperOpener_exe\\shell\\open\\command",
+		0,
+		KEY_WRITE,
+		&hKey
+		);
+
+	if (lRv != ERROR_SUCCESS)
+	{
+		DWORD dwDisposition;
+
+		cout << "command is created..." << endl;
+		// Create a key if it did not exist
+		//http://msdn.microsoft.com/en-us/library/ms724844(VS.85).aspx
+		lRv = RegCreateKeyEx(
+			HKEY_CLASSES_ROOT,
+			"EsperOpener_exe\\shell\\open\\command", //"Use Multi-Byte Character Set" by using L
+			0,
+			"ESPER",
+			REG_OPTION_NON_VOLATILE,
+			KEY_ALL_ACCESS,
+			NULL,
+			&hKey,
+			&dwDisposition
+			);
+		//char* path = "C:\\Users\\wooPC\\Source\\Repos\\Esper\\Client\\EsperClient_UI\\x64\\Debug\\EsperClient.exe %1";
+		char* path = "C:\\Users\\wooPC\\Source\\Repos\\Esper\\Client\\EsperOpener\\Debug\\EsperOpener.exe %1";
+		//http://msdn.microsoft.com/en-us/library/ms724923(VS.85).aspx
+		RegSetValueEx(
+			hKey,
+			NULL,
+			0,
+			REG_SZ,
+			//reinterpret_cast<BYTE *>(&string),
+			(BYTE *)path,
+			strlen(path)
+			);
+		//http://msdn.microsoft.com/en-us/library/ms724837(VS.85).aspx
+	}
+	RegCloseKey(hKey);
+}
+
+
+
 
 void readValueFromRegistry(void)
 {
@@ -138,6 +240,7 @@ void readValueFromRegistry(void)
 int main()
 {
 	writeToRegistry();
+	writeToRegistry2();
 	readValueFromRegistry();
 	int a;
 	cin >> a;
