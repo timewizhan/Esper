2902d916151f459f26ca42f59ef96007b1c8e512
didvlwl <didvlwl@hanmail.net>
2016-03-21 00:28:20 +0900

    change json lib

--- Client/EsperClient_UI/EsperClient/Communication.h
+++ Client/EsperClient_UI/EsperClient/Communication.h
@@ -1,15 +1,16 @@
 #pragma once
 #define WIN32_LEAN_AND_MEAN
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
 // windows.h 파일에서 자주 사용하지 않은 요소를 컴파일에서 제외시킨다.
 // 컴파일 속도가 빨라지고 winsock2.h와의 충돌이 일어나지 않는다.
+
 #include <iostream>
 #include <windows.h>
 #include <winsock2.h>
 #include <cassert>
 #include <string>
 #include <vector>
+#include "../../json/json.h"
 using namespace std;
 #pragma comment ( lib, "ws2_32.lib" )
 #pragma pack( 1 ) // 구조체에서 1byte 단위로 메모리를 Align하라는 지시어
